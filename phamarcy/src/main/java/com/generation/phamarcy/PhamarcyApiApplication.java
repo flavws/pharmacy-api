e34bffaf46279b6715da004b40ea6a0f6b648c24
flavws <persona.fmaria@gmail.com>
2023-04-19 17:34:31 -0300

    settings commit

---
+++ phamarcy/src/main/java/com/generation/phamarcy/PhamarcyApiApplication.java
@@ -0,0 +1,13 @@
+package com.generation.phamarcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarcyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamarcyApiApplication.class, args);
+	}
+
+}
---
+++ phamarcy/src/test/java/com/generation/phamarcy/PhamarcyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.phamarcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarcyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
